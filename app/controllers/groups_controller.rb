36ac452b1b1e23afe0f9ccd00fd2b1408e97e2f1
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-01-06 14:21:23 +0900

    メッセージ投稿　更新

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,14 +17,15 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-
+  
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
   end
+
 
   private
   def group_params
