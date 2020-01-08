ea5d06c8df8dbf7cc66ccc11bfb9e3e2aadff1e5
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-01-08 19:45:56 +0900

    インクリメンタルサーチ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
