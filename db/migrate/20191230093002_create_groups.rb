788268ae080c096a1d4c0459fd9c6f855ff5b848
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-01-10 12:12:38 +0900

    Update 20191230093002_create_groups.rb

--- db/migrate/20191230093002_create_groups.rb
+++ db/migrate/20191230093002_create_groups.rb
@@ -1,4 +1,4 @@
-class CreateGuoups < ActiveRecord::Migration[5.0]
+class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
